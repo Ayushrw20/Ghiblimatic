eaa758cb106177b5a3207119331cb843d1fcc789
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2025-03-30 18:05:58 +0530

    new update

--- src/app/components/GalleryGrid.tsx
+++ src/app/components/GalleryGrid.tsx
@@ -1,22 +1,25 @@
 function GalleryGrid({data}: any) {
+
+    const handleImageClick = (username: string) => {
+      window?.open('https://www.instagram.com/' + username, '_blank')?.focus();
+    }
+      
     return (        
-        <div className="grid grid-cols-3 md:grid-cols-4 gap-4 p-4">
+        <div className="columns-2 md:columns-3 lg:columns-4 gap-4 p-4 space-y-4">
         {data.map((image: any, index: any) => (
-            <div
-            key={index}
-            className="relative bg-gray-200 rounded-xl overflow-hidden 
-            shadow-md"
-            >
-            <img
-                src={image.image}
-                className="w-full h-52 object-cover"
-            />
-            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-transparent p-3 text-white text-sm">
-                {image.username}
-            </div>
-            </div>
-        ))}
+        <div key={index} className="relative overflow-hidden rounded-xl shadow-md" onClick={() => handleImageClick(image.insta)}>
+          <img
+            src={image.image}
+            className="w-full object-cover rounded-xl"
+            loading="lazy"
+          />
+          {/* Username Overlay */}
+          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/70 to-transparent p-3 text-white text-sm">
+            {image.insta}
+          </div>
         </div>
+      ))}
+    </div>
     )
 }
 
